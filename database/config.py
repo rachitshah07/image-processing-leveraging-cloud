53259b0df0437faf76e76304f0732e07a8bde216
rachitshah07 <rachitshah1525@gmail.com>
2025-02-27 18:53:02 +0530

    Configure CloudSQL connection settings in config.py for seamless Cloud Run deployment

--- database/config.py
+++ database/config.py
@@ -5,19 +5,32 @@ import psycopg2
 from psycopg2 import OperationalError, Error
 from dotenv import load_dotenv
 import urllib.parse
+from google.cloud.sql.connector import Connector, IPTypes
+import pg8000
+import sqlalchemy
+
 
 BASE_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), "..", ".env"))
-load_dotenv(BASE_DIR)
+if os.path.exists(BASE_DIR):
+    load_dotenv(BASE_DIR)
 
 
 DB_USER = os.getenv("DB_USER", "postgres")
 DB_PASS = os.getenv("DB_PASS", "password")
 DB_HOST = os.getenv("DB_HOST", "localhost")
 DB_NAME = os.getenv("DB_NAME", "image_processing")
+INSTANCE_CONNECTION_NAME = os.getenv("INSTANCE_CONNECTION_NAME")
+LOCAL_ENV = os.getenv("ENV", "true").lower() == "true" 
+
 encoded_password = urllib.parse.quote_plus(DB_PASS)
 
-DB_URL = f"postgresql://{DB_USER}:{encoded_password}@{DB_HOST}/{DB_NAME}"
+Base = declarative_base()
+SessionLocal = None
+engine = None
+
+
 def create_database_if_not_exists():
+    """Creates local database if not exists"""
     try:
         connection = psycopg2.connect(
             dbname="postgres", user=DB_USER, password=DB_PASS, host=DB_HOST
@@ -32,23 +45,48 @@ def create_database_if_not_exists():
             print(f"Database '{DB_NAME}' created successfully.")
         else:
             print(f"Database '{DB_NAME}' already exists.")
-        
+
         cursor.close()
         connection.close()
-    except OperationalError as e:
-        print(f"Error connecting to PostgreSQL: {e}")
-    except Error as e:
-        print(f"Database error: {e}")
+    except (OperationalError, Error) as e:
+        print(f"Database connection error: {e}")
 
-create_database_if_not_exists()
 
-try:
-    engine = create_engine(DB_URL)
-    print(" SQLAlchemy Engine created successfully.")
-except Exception as e:
-    print(f"Failed to create SQLAlchemy Engine: {e}")
+def connect_with_connector():
+    """Connect to Cloud SQL with Google Cloud SQL Connector"""
+    ip_type =IPTypes.PUBLIC
+    connector = Connector(refresh_strategy="LAZY")
+
+    def getconn() -> pg8000.dbapi.Connection:
+        conn: pg8000.dbapi.Connection = connector.connect(
+            instance_connection_string=INSTANCE_CONNECTION_NAME,
+            driver="pg8000",
+            user=DB_USER,
+            password=DB_PASS,
+            db=DB_NAME,
+            ip_type=ip_type,
+        )
+        return conn
+
+    return sqlalchemy.create_engine("postgresql+pg8000://", creator=getconn)
+
+
+if LOCAL_ENV:
+    print("Running in Local Environment")
+    DB_URL = f"postgresql://{DB_USER}:{encoded_password}@{DB_HOST}/{DB_NAME}"
+    create_database_if_not_exists()
+    try:
+        engine = create_engine(DB_URL)
+        print("Local SQLAlchemy Engine created successfully.")
+    except Exception as e:
+        print(f"Failed to create Local Engine: {e}")
+
+else:
+    print("Running in Cloud Environment")
+    try:
+        engine = connect_with_connector()
+        print("Cloud SQL Engine created successfully.")
+    except Exception as e:
+        print(f"Failed to create Cloud SQL Engine: {e}")
 
 SessionLocal = sessionmaker(bind=engine, expire_on_commit=False)
-
-
-Base = declarative_base()
